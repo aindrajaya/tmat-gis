7d267fe510d1e6888a488b5f5024aa8ac04e93e2
aindrajaya <arista.indrajaya@gmail.com>
2026-03-08 01:20:03 +0700

    feat: update BarChartCondition and StatusTrendChart to include offline data and adjust TMATTrendChart header

--- components/charts/TMATTrendChart.tsx
+++ components/charts/TMATTrendChart.tsx
@@ -11,7 +11,7 @@ const TMATTrendChart = forwardRef<HTMLDivElement, TMATTrendChartProps>(({ data }
 
   return (
     <div ref={ref} className="bg-white p-5 rounded-xl border border-slate-200 shadow-sm h-80">
-      <h3 className="font-semibold text-slate-700 mb-4">{t('dashboard:charts.tmatTrend')} 123</h3>
+      <h3 className="font-semibold text-slate-700 mb-4">{t('dashboard:charts.tmatTrend')}</h3>
       <ResponsiveContainer width="100%" height="90%">
         <LineChart data={data}>
           <CartesianGrid strokeDasharray="3 3" vertical={false} />
